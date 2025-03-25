62790d7d4661114a7b2224f1783645b92d5dd347
Abbo17 <31542637+Abbo17@users.noreply.github.com>
2025-03-25 01:14:34 -0300

    Create MessageSender.cs

---
+++ Handlers/MessageSender.cs
@@ -0,0 +1,25 @@
+using System.Net.WebSockets;
+using Google.Protobuf;
+using Messages.Auth;
+using ProtosCommon;
+using VentusServer;
+
+public class MessageSender
+{
+    private readonly WebSocketServerController _websocket;
+    public MessageSender(WebSocketServerController webSocket)
+    {
+        _websocket = webSocket;
+    }
+    public void SendAuthResponse(string userId, bool success)
+    {
+        ServerMessage serverMessage = new ServerMessage();
+        AuthResponse authResponse = new AuthResponse
+        {
+            Success = success
+        };
+
+        _websocket.SendServerPacketBySocket(userId, serverMessage);
+    }
+
+}
